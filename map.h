146441927920432fb53eb39109c247b13fe9f5a0
yeleyi <167627264+yeleyi@users.noreply.github.com>
2024-04-21 00:42:48 +0800

    Update map.h

--- map.h
+++ map.h
@@ -18,15 +18,15 @@ void map(){
     else
       continue;
   }
-  int ** map = new int * [row + 2];
+  string ** map = new string * [row + 2];
   for (int i = 0; i < row + 2; i++)
-    map[i] = new int [column + 2];
+    map[i] = new string [column + 2];
   for (int i = 0; i < row + 2; i++){
     for (int j = 0; j < column + 2; j++){
       if (i = 0 || i = row + 1 || j = 0 || j = column + 1)
-        map[row][column] = 1;
+        map[row][column] = "#";
       else
-        map[row][column] = 0;
+        map[row][column] = ".";
     }
   }
 }
