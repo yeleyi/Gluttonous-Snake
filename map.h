3ba37a3f57534d3cc97fe1e021df851540a71fc6
yeleyi <167627264+yeleyi@users.noreply.github.com>
2024-04-20 23:31:39 +0800

    Update map.h

--- map.h
+++ map.h
@@ -1,1 +1,21 @@
+#ifndef MAP_H
+#define MAP_H
+#include <string>
+cout << "How many rows would you like?" << endl;
+int row, column;
+cin >> row;
+cout << "How many columns would you like?" << endl;
+cin >> column;
+int ** map = new int * [row + 2];
+for (int i = 0; i < row + 2; i++)
+  map[i] = new int [column + 2];
+for (int i = 0; i < row + 2; i++){
+  for (int j = 0; j < column + 2; j++){
+    if (i = 0 || i = row + 1 || j = 0 || j = column + 1)
+      map[row][column] = 1;
+    else
+      map[row][column] = 0;
+  }
+}
 
+#endif
