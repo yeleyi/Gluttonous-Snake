fd80b8d5700fdf1fb4e849367ecaaebccf05dac6
Pzmm0808 <158024565+Pzmm0808@users.noreply.github.com>
2024-04-21 02:04:47 +0800

    Update randomBeam.h

--- randomBeam.h
+++ randomBeam.h
@@ -1,25 +1,29 @@
 #ifndef RANDOM_BEAM_H
 #define RANDOM_BEAM_H
 
-#include <stdlib.h>
-#include <time.h>
+#include <iostream>
+#include <ctime>
+
+int beansCount = 0;
 
 // 随机生成数字的函数
 int randomInt(int min, int max){
-  return min + rand() / (RAND_MAX / (max - min + 1) + 1);
+  return min + rand() % (max -min + 1);
 }
 // 随机在地图上生成豆子的函数
-void generateBean(int width, int height, int playerX, int playerY, char map[width][height]){
-  int beanX, beanY;
+void RandomBeam::generateBean() {
+    if (beansCount_ >= 3) {
+        return;
+    }
 
-  // 生成随机豆子的位置
-  do {
-    beanX = randomInt(0, width - 1);
-    beanY = randomInt(0, height - 1);
-  } while (map[beanX][beanY] != '#' || isPlayerPosition(playerX, playerY, beanX, beanY));
+    int beanX, beanY;
+    do {
+        beanX = randomInt(0, width_ - 1);
+        beanY = randomInt(0, height_ - 1);
+    } while (map_[beanX][beanY] != ' ' || isPlayerPosition(beanX, beanY));
 
-  // 放置B
-  map[beanX][beanY] = 'B';
+    map_[beanX][beanY] = 'B'; // Assuming 'B' represents a bean
+    ++beansCount_;
 }
 
 // 判断生成地点是否有Player
