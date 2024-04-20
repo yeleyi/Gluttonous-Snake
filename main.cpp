420404ed7c4bdcbb40ab53c5b47de6297590796e
Pzmm0808 <158024565+Pzmm0808@users.noreply.github.com>
2024-04-21 01:17:16 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <string>
+#include <thread>
+#include <chrono>
 #include "map.h"
 #include "movePlayer.h"
 using namespace std;
@@ -24,7 +26,21 @@ int main(){
   map(width, height);
   int playerX = width/2;
   int playerY = height/2;
-  movePlayer(width, height, playerX, playerY);
+
+  renderMap(); // 初始渲染地图
+
+  //游戏循环
+  while (true){
+    // 每个循环休眠一段时间，模拟游戏速度
+    std::this_thread::sleep_for(std::chrono:milliseconds(200));
+
+    // 移动玩家
+    movePlayer(width, height, playerX, playerY);
+    renderMap(); // 更新并重新渲染地图
+  }
+  
+  
+  
   for (int i = 0; i < row + 2; i++)
     delete[] map[i];
   delete[] map;
