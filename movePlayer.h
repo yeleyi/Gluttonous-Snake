c2667c57e235e9d55085d9e06fab3217a892c29f
yeleyi <167627264+yeleyi@users.noreply.github.com>
2024-04-21 02:15:17 +0800

    Update movePlayer.h

--- movePlayer.h
+++ movePlayer.h
@@ -23,30 +23,38 @@ int dy=-1; //初始向上移动
 
 void movePlayer(int width, int height, int playerX, int playerY){
   //更新玩家位置
-  playerX += dx;
-  playerY += dy;
   string command;
-  cin >> command;
-  while (cin >> command){
-    if (command == "a")
-      playerY -= 1;
-    else if (command == "d")
-      playerY += 1;
-    else if (command == "w")
-      playerX -= 1;
-    else if (command == "s")
-      playerX += 1;
-  // 检查是否撞墙
-    if (map[playerY][playerX] == '#'){
-      // 撞墙，游戏结束
-        break;
-        endwin();
-        std::cout << "Game Over!" << std::endl;
-        exit(0);
+  while true{
+    if (cin >> command){
+      if (map[playerY][playerX] == '#')
+      if (command == "a" or command == "A")
+        playerY -= 1;
+      else if (command == "d" or command == "D")
+        playerY += 1;
+      else if (command == "w" or command == "W")
+        playerX -= 1;
+      else if (command == "s" or command == "S")
+        playerX += 1;
+    // 检查是否撞墙
+    else{
+      playerX += dx;
+      playerY += dy;
     }
+      if (map[playerY][playerX] == '#'){
+        // 撞墙，游戏结束
+          endwin();
+          std::cout << "Game Over!" << std::endl;
+          break;
+          exit(0);
+      }
+      if (map[playerY][playerX] == "P"){
+        cout << "Game Over!" << endl;
+        break;
+      }
   // 更新地图
-  map[playerY - dy][playerX - dx] = '.';
-  map[playerY][playerX] = 'P';
+    map[playerY - dy][playerX - dx] = '.';
+    map[playerY][playerX] = 'P';
 }
+    }
 
 #endif
