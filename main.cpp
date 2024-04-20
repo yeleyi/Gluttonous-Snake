4aa8819a6a0c526cd3e6bda6b09e3c0b7d7851e0
yeleyi <167627264+yeleyi@users.noreply.github.com>
2024-04-20 23:21:01 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,22 +4,7 @@
 using namespace std;
 
 int main(){
-  cout << "How many rows would you like?" << endl;
-  int row, column;
-  cin >> row;
-  cout << "How many columns would you like?" << endl;
-  cin >> column;
-  int ** map = new int * [row + 2];
-  for (int i = 0; i < row + 2; i++)
-    map[i] = new int [column + 2];
-  for (int i = 0; i < row + 2; i++){
-    for (int j = 0; j < column + 2; j++){
-      if (i = 0 || i = row + 1 || j = 0 || j = column + 1)
-        map[row][column] = 1;
-      else
-        map[row][column] = 0;
-    }
-  }
+  
   
   
   for (int i = 0; i < row + 2; i++)
