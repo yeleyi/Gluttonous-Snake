ac962ea88edd128a27e0475e75df5e1489500013
yeleyi <167627264+yeleyi@users.noreply.github.com>
2024-04-21 00:55:56 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,22 @@
 using namespace std;
 
 int main(){
+  int width, height;
+  cout << "How wide do you want your map to be?" << endl;
+  cin >> width;
+  cout << "How high do you want your map to be?" << endl;
+  cin >> height;
+  while (width <  || height < ){
+    cout << "The map is too small. Please input again." << endl;
+    cout << "How wide do you want your map to be?" << endl;
+    cin >> width;
+    cout << "How wide do you want your map to be?" << endl;
+    cin >> height;
+    if (width >=  && height >= )
+      break;
+    else
+      continue;
+  }
   map();
   
   for (int i = 0; i < row + 2; i++)
