9480ffacfa78065125e747421f82f36c149dbe85
yeleyi <167627264+yeleyi@users.noreply.github.com>
2024-04-20 23:13:40 +0800

    Update map.cpp

--- map.cpp
+++ map.cpp
@@ -9,15 +9,20 @@ int main(){
   cin >> row;
   cout << "How many columns would you like?" << endl;
   cin >> column;
-  int ** map = new int * [map];
-  for (int i = 0; i < row; i++)
-    map[i] = new int [column];
-
-
-
-
-
-  for (int i = 0; i < row; i++)
+  int ** map = new int * [row + 2];
+  for (int i = 0; i < row + 2; i++)
+    map[i] = new int [column + 2];
+  for (int i = 0; i < row + 2; i++){
+    for (int j = 0; j < column + 2; j++){
+      if (i = 0 || i = row + 1 || j = 0 || j = column + 1)
+        map[row][column] = 1;
+      else
+        map[row][column] = 0;
+    }
+  }
+  
+  
+  for (int i = 0; i < row + 2; i++)
     delete[] map[i];
   delete[] map;
   return 0;
