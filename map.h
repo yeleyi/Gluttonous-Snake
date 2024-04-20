b2ea30fc537c4dd263f145c67bcbf261613e8552
yeleyi <167627264+yeleyi@users.noreply.github.com>
2024-04-21 00:38:54 +0800

    Update map.h

--- map.h
+++ map.h
@@ -1,20 +1,33 @@
 #ifndef MAP_H
 #define MAP_H
 #include <string>
-cout << "How many rows would you like?" << endl;
-int row, column;
-cin >> row;
-cout << "How many columns would you like?" << endl;
-cin >> column;
-int ** map = new int * [row + 2];
-for (int i = 0; i < row + 2; i++)
-  map[i] = new int [column + 2];
-for (int i = 0; i < row + 2; i++){
-  for (int j = 0; j < column + 2; j++){
-    if (i = 0 || i = row + 1 || j = 0 || j = column + 1)
-      map[row][column] = 1;
+void map(){
+  int row, column;
+  cout << "How many rows would you like?" << endl;
+  cin >> row;
+  cout << "How many columns would you like?" << endl;
+  cin >> column;
+  while (row <  || column < ){
+    cout << "The map is too small. Please input again." << endl;
+    cout << "How many rows would you like?" << endl;
+    cin >> row;
+    cout << "How many columns would you like?" << endl;
+    cin >> column;
+    if (row >=  && column >= )
+      break;
     else
-      map[row][column] = 0;
+      continue;
+  }
+  int ** map = new int * [row + 2];
+  for (int i = 0; i < row + 2; i++)
+    map[i] = new int [column + 2];
+  for (int i = 0; i < row + 2; i++){
+    for (int j = 0; j < column + 2; j++){
+      if (i = 0 || i = row + 1 || j = 0 || j = column + 1)
+        map[row][column] = 1;
+      else
+        map[row][column] = 0;
+    }
   }
 }
 
