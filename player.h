90a56d3f7ad1a951fc26cf6cc7af035c3a2f3665
yeleyi <167627264+yeleyi@users.noreply.github.com>
2024-04-25 19:21:21 +0800

    Update player.h

--- player.h
+++ player.h
@@ -4,12 +4,15 @@
 #include <vector>
 #include "map.h"
 
+using namespace std;
+
 struct Player{
   std::vector<std::pair<int, int>> body; //玩家的身体，每个元素表示一个身体部位的坐标
 };
 
 // 当玩家触碰豆子时调用，扩展玩家长度
   void extendPlayerLength(Player &player) {
+    if 
     player.body.push_back(player.body.back());
   }
 
