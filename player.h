f5ab430ca0f2ea3d25e2021347a145182f700674
Pzmm0808 <158024565+Pzmm0808@users.noreply.github.com>
2024-04-25 13:21:02 +0800

    Update player.h

--- player.h
+++ player.h
@@ -8,14 +8,9 @@ struct Player{
   std::vector<std::pair<int, int>> body; //玩家的身体，每个元素表示一个身体部位的坐标
 };
 
-class ExtendLength {
-public:
-    ExtendLength() {}
-
-    // 当玩家触碰豆子时调用，扩展玩家长度
-    void extendPlayerLength(Player &player) {
-      player.body.push_back(player.body.back());
-    }
-};
+// 当玩家触碰豆子时调用，扩展玩家长度
+  void extendPlayerLength(Player &player) {
+    player.body.push_back(player.body.back());
+  }
 
 #endif
