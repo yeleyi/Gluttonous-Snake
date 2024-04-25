6c9bf4aa212ed0333ee5a61429bfe6cc2eb33814
Pzmm0808 <158024565+Pzmm0808@users.noreply.github.com>
2024-04-26 03:31:55 +0800

    Update map.h

--- map.h
+++ map.h
@@ -1,19 +1,8 @@
 #ifndef MAP_H
 #define MAP_H
 #include <string>
+
 //Initialized the map with "#" at the edge and "." in the middle
-void mapf(int width, int height, string ** map){
-  string ** map = new string * [width + 2];
-  for (int i = 0; i < width + 2; i++)
-    map[i] = new string [height + 2];
-  for (int i = 0; i < width + 2; i++){
-    for (int j = 0; j < height + 2; j++){
-      if (i == 0 || i == width + 1 || j == 0 || j == height + 1)
-        map[width][height] = "#";
-      else
-        map[width][height] = ".";
-    }
-  }
-}
+void mapf(int width, int height, std::string ** map);
 
 #endif
