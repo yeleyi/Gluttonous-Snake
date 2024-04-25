ada17d1cd1c0960b54fe5e4ef05aa4088a469f35
Pzmm0808 <158024565+Pzmm0808@users.noreply.github.com>
2024-04-26 03:34:34 +0800

    Update renderMap.h

--- renderMap.h
+++ renderMap.h
@@ -1,17 +1,9 @@
 #ifndef RENDERMAP_H
 #define RENDERMAP_H
 #include <ncurses.h>
+#include <string>
 
 // 渲染地图
-void renderMap(int width, int height, char map){
-  clear();
-  for (int y=0; y<height; ++y){
-    for (int x=0; x<width; ++x){
-      printw("%c", map[y][x]);
-    }
-    printw("\n");
-  }
-  refresh(); // 刷新屏幕
-}
+void renderMap(int width, int height, std::string **map);
 
 #endif
