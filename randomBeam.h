0e79b97851928883063558685870338999def9cb
Pzmm0808 <158024565+Pzmm0808@users.noreply.github.com>
2024-04-25 13:15:09 +0800

    Update randomBeam.h

--- randomBeam.h
+++ randomBeam.h
@@ -16,17 +16,17 @@ void generateBean(int width, int height, const Player &P, char map[width][height
     int beanX, beanY;
 
     // 随机生成bean的位置
-    do {
-        beanX = randomInt(0, width - 1);
-        beanY = randomInt(0, height - 1);
-    } while (map[beanX][beanY] != '#' || isPlayerPosition(P.body[0].first, P.body[0].second, beanX, beanY));
-
+    if (map[beanX][beanY] != '#' && isPlayerPosition(P.body[0].first, P.body[0].second, beanX, beanY)){
+      beanX = randomInt(0, width-1);
+      beanY = randomInt(0, height-1);
+    }
     // 放置bean
-    map[beanX][beanY] = 'B'; // Assuming 'B' represents a bean
+    map[beanX][beanY] = 'B';
 }
 
 // 判断生成地点是否有Player
 int isPlayerPosition(int playerX, int playerY, int beanX, int beanY){
-  return (playerX == beanX && playerY == beanY);
+  return (playerX != beanX && playerY != beanY);
 }
+
 #endif
