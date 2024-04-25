01f1cd424d298726b5330f60fa009fd9c258ecd9
Pzmm0808 <158024565+Pzmm0808@users.noreply.github.com>
2024-04-26 07:50:57 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -27,20 +27,24 @@ int main(){
     else
       continue;
   }
+    char command, lastcommand;
+    int round=0;
+    int dirX = 0;
+    int dirY = -1;
     // 初始化地图数组
-  string command = "0", lastcommand = "0";
-  int round = 0;
-  string **map = new string *[width + 2];
-  for (int i = 0; i < width + 2; i++)
-        map[i] = new string[height + 2];
+    char **map = new char *[width + 2];
+    for (int i = 0; i < width + 2; i++)
+        map[i] = new char[height + 2];
   Player P; //创建一个Player P
   P.body.push_back(make_pair(width/2, height/2));
+  cout << P.body[0].first << endl << P.body[0].second << endl;
   mapf(width, height, map);
   int beansCount = 0;
-
   initscr(); // 初始化 ncurses
   cbreak(); // 禁用行缓冲（让按键立即生效）
   noecho(); // 关闭回显
+
+  nodelay(stdscr, true);
 
   renderMap(width, height, map); // 初始渲染地图
 
@@ -48,9 +52,9 @@ int main(){
   while (true){
     // 每个循环休眠一段时间，模拟游戏速度
     std::this_thread::sleep_for(std::chrono::milliseconds(200));
-
     // 移动玩家
-    movePlayer(width, height, P, map, round, command, lastcommand);
+    int ch = getch();
+    movePlayer(width, height, P, map, command, lastcommand, round, ch, dirX, dirY);
 
     // 检查地图上是否有bean
     if (beansCount < 1) {
@@ -59,18 +63,19 @@ int main(){
     }
 
     // 检查是否碰撞B
-    if (map[P.body[0].first][P.body[0].second] == "B"){
+    if (map[P.body[0].first][P.body[0].second] == 'B'){
       beansCount--;
       extendPlayerLength(P);
     }
-    
+
     renderMap(width, height, map); // 更新并重新渲染地图
   }
-  
-  
-  
+
+
+
   for (int i = 0; i < width + 2; i++)
     delete[] map[i];
   delete[] map;
+  endwin();
   return 0;
 }
