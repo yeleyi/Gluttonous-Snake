9fa22757e6113834c83ac17edaa442f778195d62
Pzmm0808 <158024565+Pzmm0808@users.noreply.github.com>
2024-04-26 07:55:40 +0800

    Update movePlayer.cpp

--- movePlayer.cpp
+++ movePlayer.cpp
@@ -1,84 +1,61 @@
 #include "movePlayer.h"
 #include <iostream>
 
-using namespace std;
-
-void movePlayer(int width, int height, Player &P, std::string **map, int &round, string &command, string &lastcommand){
-  //更新玩家位置
-  int round = 0;
-  while (true){
-    std::string command = getch();
-      round = 1;
-      if (lastcommand == "d"){
-        if (command == "a")
-          P.body[0].first += 1;
-        else if (command == "d")
-          P.body[0].first += 1;
-        else if (command == "w")
-          P.body[0].second -= 1;
-        else if (command == "s")
-          P.body[0].second += 1;
-      }
-      if (lastcommand == "a"){
-        if (command == "a")
-          P.body[0].first -= 1;
-        else if (command == "d")
-          P.body[0].first -= 1;
-        else if (command == "w")
-          P.body[0].second -= 1;
-        else if (command == "s")
-          P.body[0].second += 1;
-      }
-      if (lastcommand == "w"){
-        if (command == "a")
-          P.body[0].first -= 1;
-        else if (command == "d")
-          P.body[0].first += 1;
-        else if (command == "w")
-          P.body[0].second -= 1;
-        else if (command == "s")
-          P.body[0].second -= 1;
-      }
-      if (lastcommand == "s"){
-        if (command == "a")
-          P.body[0].first -= 1;
-        else if (command == "d")
-          P.body[0].first += 1;
-        else if (command == "w")
-          P.body[0].second += 1;
-        else if (command == "s")
-          P.body[0].second += 1;
-      }
-      lastcommand = command;
-    }
-    else{
-      round = 0;
-      if (lastcommand == "w")
-        P.body[0].second -= 1;
-      if (lastcommand == "d")
-        P.body[0].first += 1;
-      if (lastcommand == "s")
-        P.body[0].second += 1;
-      if (lastcommand == "a")
-        P.body[0].first -= 1;
-    }
-    
-    if (map[P.body[0].first][P.body[0].second] == "#"){
+void movePlayer(int width, int height, Player &P, char **map, char command, char lastcommand, int round,int ch, int &dirX, int &dirY){
+      if (ch != ERR) {
+            switch (ch) {
+                case 'w':
+                    // 上移逻辑
+                        std::cout << "检测到w移动"<<std::endl;
+                    if (dirX!=0){
+                        dirX=0;
+                        dirY=-1;
+                    }
+                    break;
+                case 's':
+                    // 下移逻辑
+                    std::cout << "检测到s移动"<<std::endl;
+                    if (dirX!=0){
+                        dirX=0;
+                        dirY=1;
+                    }
+                    break;
+                case 'a':
+                    // 左移逻辑
+                    std::cout << "检测到a移动"<<std::endl;
+                    if (dirY!=0){
+                        dirX=-1;
+                        dirY=0;
+                    }
+                    break;
+                case 'd':
+                    // 右移逻辑
+                    std::cout << "检测到d移动"<<std::endl;
+                    if (dirY!=0){
+                        dirX=1;
+                        dirY=0;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            flushinp();
+        }
+    P.body[0].first += dirX;
+    P.body[0].second += dirY;
+    if (map[P.body[0].second][P.body[0].first] == '#'){
       // 撞墙，游戏结束
         endwin();
         std::cout << "Game Over!" << std::endl;
         exit(0);
     }
-    if (map[P.body[0].first][P.body[0].second] == "P"){
+   /* if (map[P.body[0].first][P.body[0].second] == 'P'){
       //撞到自身，游戏结束
       cout << "Game Over!" << endl;
         exit(0);
-      }
-
-    
-  // 更新地图
-    map[P.body[0].first - 1][P.body[0].second] = ".";
-      map[P.body[0].first][P.body[0].second] = "P";
+      }*/
+    map[P.body[0].second][P.body[0].first] = 'P';
+    map[P.body[0].second-dirY][P.body[0].first-dirX] = '.';
     lastcommand = command;
-  }
 }
+
