819c9ed5a82a60b4fc767d03f912c821cb16dda3
Pzmm0808 <158024565+Pzmm0808@users.noreply.github.com>
2024-04-26 07:56:35 +0800

    Update movePlayer.h

--- movePlayer.h
+++ movePlayer.h
@@ -3,22 +3,11 @@
 #include "map.h"
 #include "player.h"
 #include <ncurses.h>
-#include <conio.h>
 #include <string>
 
 using namespace std;
 
-// 地图大小
-// const int width;
-// const int height;
 
-// 地图
-// char map[height][width];
-
-// 玩家初始位置
-// int playerX = width/2;
-// int playerY = height/2;
-
-void movePlayer(int width, int height, Player &P, std::string **map);
+void movePlayer(int width, int height, Player &P, char **map, char command, char lastcommand, int round,int ch, int &dirX, int &dirY);
 
 #endif
