fa70ebf6822542d654f125c7f0d697b372a4241a
Pzmm0808 <158024565+Pzmm0808@users.noreply.github.com>
2024-04-26 03:34:00 +0800

    Update randomBeam.h

--- randomBeam.h
+++ randomBeam.h
@@ -3,28 +3,14 @@
 
 #include <iostream>
 #include <ctime>
-#include "player.h"
+#include "player.hpp"
 
 // 随机生成数字的函数
-int randomInt(int min, int max){
-  return min + rand() % (max -min + 1);
-}
-// 随机在地图上生成豆子的函数
-void generateBean(int width, int height, const Player &P, char map[width][height]) {
-    int beanX, beanY;
+int randomInt(int min, int max);
 
-    // 随机生成bean的位置
-    if (map[beanX][beanY] != '#' && isPlayerPosition(P.body[0].first, P.body[0].second, beanX, beanY)){
-      beanX = randomInt(0, width-1);
-      beanY = randomInt(0, height-1);
-    }
-    // 放置bean
-    map[beanX][beanY] = 'B';
-}
+// 随机在地图上生成豆子的函数
+void generateBean(int width, int height, const Player &P, std::string **map);
 
 // 判断生成地点是否有Player
-int isPlayerPosition(int playerX, int playerY, int beanX, int beanY){
-  return (playerX != beanX && playerY != beanY);
-}
-
+int isPlayerPosition(int playerX, int playerY, int beanX, int beanY);
 #endif
