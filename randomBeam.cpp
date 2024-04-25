91efdb1ded6741b0533e516df49abe6eb99ad170
Pzmm0808 <158024565+Pzmm0808@users.noreply.github.com>
2024-04-26 07:57:17 +0800

    Update randomBeam.cpp

--- randomBeam.cpp
+++ randomBeam.cpp
@@ -1,16 +1,16 @@
 #include "randomBeam.h"
-
+  
 int randomInt(int min, int max){
   return min + rand() % (max -min + 1);
 }
 // 随机在地图上生成豆子的函数
-void generateBean(int width, int height, const Player &P, std::string **map) {
+void generateBean(int width, int height, const Player &P, char **map) {
     int beanX, beanY;
 
     do{
         beanX = randomInt(1, width -2);
         beanY = randomInt(1, height-2);
-    } while (map[beanX][beanY] != ".");
+    } while (map[beanX][beanY] != '.');
     // 放置bean
     map[beanX][beanY] = 'B';
 }
@@ -19,3 +19,4 @@ void generateBean(int width, int height, const Player &P, std::string **map) {
 int isPlayerPosition(int playerX, int playerY, int beanX, int beanY){
   return (playerX != beanX && playerY != beanY);
 }
+
