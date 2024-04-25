249b65a27e3d91e83f826c7770a34cb7324a80b1
Pzmm0808 <158024565+Pzmm0808@users.noreply.github.com>
2024-04-26 03:33:27 +0800

    Update player.h

--- player.h
+++ player.h
@@ -2,9 +2,9 @@
 #define PLAYER_H
 
 #include <vector>
-#include "map.h"
-#include "movePlayer.h"
+#include "map.hpp"
 #include <string>
+
 using namespace std;
 
 struct Player{
@@ -12,46 +12,6 @@ struct Player{
 };
 
 // 当玩家触碰豆子时调用，扩展玩家长度
-void extendPlayerLength(Player &P) { 
-  string command, lastcommand;
-  int round;
-  P.body.push_back(P.body.back());
-  for (int i == 1; i < P.body.size(); i++)
-    P.body[i] = P.body[i - 1];
-  if (round == 1){
-    if (command == "w"){
-      P.body[0].first = P.body[1].first;
-      P.body[0].second = P.body[1].second - 1;
-    }
-    if (command == "s"){
-      P.body[0].first = P.body[1].first;
-      P.body[0].second = P.body[1].second + 1;
-    }
-    if (command == "a"){
-      P.body[0].second = P.body[1].second;
-      P.body[0].first = P.body[1].first - 1;
-    }
-    if (command == "d"){
-      P.body[0].second = P.body[1].second;
-      P.body[0].first = P.body[1].first + 1;
-    }
-  }
-  else if (round == 0){
-    if (last command == "w"){
-      P.body[0].first = P.body[1].first;
-      P.body[0].second = P.body[1].second - 1;
-    }
-    if (last command == "s"){
-      P.body[0].first = P.body[1].first;
-      P.body[0].second = P.body[1].second + 1;
-    }
-    if (last command == "a"){
-      P.body[0].second = P.body[1].second;
-      P.body[0].first = P.body[1].first - 1;
-    }
-    if (last command == "d"){
-      P.body[0].second = P.body[1].second;
-      P.body[0].first = P.body[1].first + 1;
-    }
-}
+void extendPlayerLength(Player &P);
+
 #endif
