09d1512da52f591f0d92bde9b39452a10f9366e5
Pzmm0808 <158024565+Pzmm0808@users.noreply.github.com>
2024-04-26 07:58:04 +0800

    Update renderMap.cpp

--- renderMap.cpp
+++ renderMap.cpp
@@ -1,12 +1,14 @@
 #include "renderMap.h"
+#include <iomanip>
 
-void renderMap(int width, int height, std::string **map){
+void renderMap(int width, int height, char **map){
   clear();
   for (int y=0; y<height; ++y){
     for (int x=0; x<width; ++x){
-      printw("%c", map[y][x].c_str());
+            printw("%c", map[y][x]);
     }
     printw("\n");
   }
   refresh(); // 刷新屏幕
 }
+
