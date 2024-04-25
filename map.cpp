527587df91f230c34bb63e27d622958af8ac31d6
Pzmm0808 <158024565+Pzmm0808@users.noreply.github.com>
2024-04-26 03:32:20 +0800

    Create map.cpp

---
+++ map.cpp
@@ -0,0 +1,14 @@
+#include "map.hpp"
+void mapf(int width, int height, std::string ** map){
+  map = new std::string * [width + 2];
+  for (int i = 0; i < width + 2; i++)
+    map[i] = new std::string [height + 2];
+  for (int i = 0; i < width + 2; i++){
+    for (int j = 0; j < height + 2; j++){
+      if (i == 0 || i == width + 1 || j == 0 || j == height + 1)
+        map[width][height] = "#";
+      else
+        map[width][height] = ".";
+    }
+  }
+}
