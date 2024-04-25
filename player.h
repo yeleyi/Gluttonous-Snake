7bc9e45aaa690fb6753bb325bcffd19917f50633
yeleyi <167627264+yeleyi@users.noreply.github.com>
2024-04-26 03:03:47 +0800

    Update player.h

--- player.h
+++ player.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include "map.h"
+#include "movePlayer.h"
 #include <string>
 using namespace std;
 
@@ -11,45 +12,46 @@ struct Player{
 };
 
 // 当玩家触碰豆子时调用，扩展玩家长度
-  void extendPlayerLength(Player &P) { 
-    P.body.push_back(P.body.back());
-    for (int i == 1; i < P.body.size(); i++)
-      P.body[i] = P.body[i - 1];
-    if (round == 1){
-      if (command == "w"){
-        P.body[0].first = P.body[1].first;
-        P.body[0].second = P.body[1].second - 1;
-      }
-      if (command == "s"){
-        P.body[0].first = P.body[1].first;
-        P.body[0].second = P.body[1].second + 1;
-      }
-      if (command == "a"){
-        P.body[0].second = P.body[1].second;
-        P.body[0].first = P.body[1].first - 1;
-      }
-      if (command == "d"){
-        P.body[0].second = P.body[1].second;
-        P.body[0].first = P.body[1].first + 1;
-      }
+void extendPlayerLength(Player &P) { 
+  string command, lastcommand;
+  int round;
+  P.body.push_back(P.body.back());
+  for (int i == 1; i < P.body.size(); i++)
+    P.body[i] = P.body[i - 1];
+  if (round == 1){
+    if (command == "w"){
+      P.body[0].first = P.body[1].first;
+      P.body[0].second = P.body[1].second - 1;
     }
-    else if (round == 0){
-      if (last command == "w"){
-        P.body[0].first = P.body[1].first;
-        P.body[0].second = P.body[1].second - 1;
-      }
-      if (last command == "s"){
-        P.body[0].first = P.body[1].first;
-        P.body[0].second = P.body[1].second + 1;
-      }
-      if (last command == "a"){
-        P.body[0].second = P.body[1].second;
-        P.body[0].first = P.body[1].first - 1;
-      }
-      if (last command == "d"){
-        P.body[0].second = P.body[1].second;
-        P.body[0].first = P.body[1].first + 1;
-      }
+    if (command == "s"){
+      P.body[0].first = P.body[1].first;
+      P.body[0].second = P.body[1].second + 1;
+    }
+    if (command == "a"){
+      P.body[0].second = P.body[1].second;
+      P.body[0].first = P.body[1].first - 1;
+    }
+    if (command == "d"){
+      P.body[0].second = P.body[1].second;
+      P.body[0].first = P.body[1].first + 1;
+    }
   }
-
+  else if (round == 0){
+    if (last command == "w"){
+      P.body[0].first = P.body[1].first;
+      P.body[0].second = P.body[1].second - 1;
+    }
+    if (last command == "s"){
+      P.body[0].first = P.body[1].first;
+      P.body[0].second = P.body[1].second + 1;
+    }
+    if (last command == "a"){
+      P.body[0].second = P.body[1].second;
+      P.body[0].first = P.body[1].first - 1;
+    }
+    if (last command == "d"){
+      P.body[0].second = P.body[1].second;
+      P.body[0].first = P.body[1].first + 1;
+    }
+}
 #endif
