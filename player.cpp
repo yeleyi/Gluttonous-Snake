51743c6f75c8d7e68b191a616614e267fe67d066
Pzmm0808 <158024565+Pzmm0808@users.noreply.github.com>
2024-04-26 03:33:44 +0800

    Create player.cpp

---
+++ player.cpp
@@ -0,0 +1,42 @@
+#include "player.hpp"
+
+void extendPlayerLength(Player &P) {
+  P.body.push_back(P.body.back());
+  for (int i = 1; i < P.body.size(); i++)
+    P.body[i] = P.body[i - 1];
+  if (round == 1){
+    if (command == "w"){
+      P.body[0].first = P.body[1].first;
+      P.body[0].second = P.body[1].second - 1;
+    }
+    if (command == "s"){
+      P.body[0].first = P.body[1].first;
+      P.body[0].second = P.body[1].second + 1;
+    }
+    if (command == "a"){
+      P.body[0].second = P.body[1].second;
+      P.body[0].first = P.body[1].first - 1;
+    }
+    if (command == "d"){
+      P.body[0].second = P.body[1].second;
+      P.body[0].first = P.body[1].first + 1;
+    }
+  }
+  else if (round == 0){
+    if (lastcommand == "w"){
+      P.body[0].first = P.body[1].first;
+      P.body[0].second = P.body[1].second - 1;
+    }
+    if (lastcommand == "s"){
+      P.body[0].first = P.body[1].first;
+      P.body[0].second = P.body[1].second + 1;
+    }
+    if (lastcommand == "a"){
+      P.body[0].second = P.body[1].second;
+      P.body[0].first = P.body[1].first - 1;
+    }
+    if (lastcommand == "d"){
+      P.body[0].second = P.body[1].second;
+      P.body[0].first = P.body[1].first + 1;
+    }
+}
