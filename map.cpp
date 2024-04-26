263d303dc0e25d4205fef0eb89696460acf9e088
Pzmm0808 <158024565+Pzmm0808@users.noreply.github.com>
2024-04-26 10:12:06 +0800

    Update map.cpp

--- map.cpp
+++ map.cpp
@@ -1,17 +1,5 @@
 #include "map.h"
 void mapf(int width, int height, char ** map){
-  //map = new std::string * [width + 2];
-  /* for (int i = 0; i < width + 2; i++){
-        //map[i] = new std::string [height + 2];
-        for (int i = 0; i < width + 2; i++){
-            for (int j = 0; j < height + 2; j++){
-                if (i == 0 || i == width + 1 || j == 0 || j == height + 1)
-                    map[width][height] = '#';
-                else
-                    map[width][height] = '.';
-            }
-        }
-    }*/
     for (int y = 0; y < height; ++y) {
         for (int x = 0; x < width; ++x) {
             map[y][x] = '.';
