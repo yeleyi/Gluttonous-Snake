e00d6ddf881613c507d011da331043f53abcf2ba
Pzmm0808 <158024565+Pzmm0808@users.noreply.github.com>
2024-04-30 02:00:01 +0800

    Update movePlayer.cpp

--- movePlayer.cpp
+++ movePlayer.cpp
@@ -3,32 +3,32 @@
 #include <chrono>
 
 using namespace std;
-  
-void movePlayer(int width, int height, Player &P, char **map,int ch, int &dirX, int &dirY, int &beansCount, int &sleep){
+
+void movePlayer(int width, int height, Player &P, char **map,int ch, int &dirX, int &dirY, int &beansCount/*int &sleep*/){
     if (ch != ERR) {
             switch (ch) {
                 case 'w':
                     // 上移逻辑
-                        std::cout << "检测到w移动"<<std::endl;
+                        //std::cout << "检测到w移动"<<std::endl;
                     if (dirX!=0){
                         dirX=0;
                         dirY=-1;
-                        
+
                     }
-                    
+
                     break;
                 case 's':
                     // 下移逻辑
-                    std::cout << "检测到s移动"<<std::endl;
+                    //std::cout << "检测到s移动"<<std::endl;
                     if (dirX!=0){
                         dirX=0;
                         dirY=1;
                     }
-                
+
                     break;
                 case 'a':
                     // 左移逻辑
-                    std::cout << "检测到a移动"<<std::endl;
+                    //std::cout << "检测到a移动"<<std::endl;
                     if (dirY!=0){
                         dirX=-1;
                         dirY=0;
@@ -36,7 +36,7 @@ void movePlayer(int width, int height, Player &P, char **map,int ch, int &dirX, 
                     break;
                 case 'd':
                     // 右移逻辑
-                    std::cout << "检测到d移动"<<std::endl;
+                   // std::cout << "检测到d移动"<<std::endl;
                     if (dirY!=0){
                         dirX=1;
                         dirY=0;
