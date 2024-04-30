af4309289cc24e977751167999b86e3b82688958
Pzmm0808 <158024565+Pzmm0808@users.noreply.github.com>
2024-04-30 20:45:06 +0800

    Update randomBeam.cpp

--- randomBeam.cpp
+++ randomBeam.cpp
@@ -1,5 +1,7 @@
 #include "randomBeam.h"
-  
+
+
+// generate a random integer for the position of the random bean
 int randomInt(int min, int max){
 static bool initialized = false;
   if (!initialized){
@@ -9,6 +11,7 @@ static bool initialized = false;
   return min + rand() % (max -min + 1);
 }
 
+// generate random bean using the random integer
 void generateBean(int width, int height, const Player &P, char **map) {
   int beanX, beanY;
 
@@ -20,7 +23,7 @@ void generateBean(int width, int height, const Player &P, char **map) {
   map[beanX][beanY] = 'B';
 }
 
-
+// check if player position = random bean position, if yes don't generate the bean on this position
 int isPlayerPosition(int playerX, int playerY, int beanX, int beanY){
   return (playerX != beanX && playerY != beanY);
 }
