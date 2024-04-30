a1db9aed446296dd3e138c7398e1ed30fe3dc178
Pzmm0808 <158024565+Pzmm0808@users.noreply.github.com>
2024-04-30 16:33:26 +0800

    Update player.h

--- player.h
+++ player.h
@@ -2,17 +2,12 @@
 #define PLAYER_H
 
 #include <vector>
-#include "map.h"
-#include <ncurses.h>
 #include <iostream>
 
 using namespace std;
 
 struct Player{
-  std::vector<std::pair<int, int>> body; //玩家的身体，每个元素表示一个身体部位的坐标
+  std::vector<std::pair<int, int>> body;
 };
-
-// 当玩家触碰豆子时调用，扩展玩家长度
-pair<int, int> extendPlayerLength(Player &P, char **map,int width, int height, int dirX, int dirY);
 
 #endif
