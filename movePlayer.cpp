e0a70e87509de29153a84e3834cd8c4fee691a9b
Pzmm0808 <158024565+Pzmm0808@users.noreply.github.com>
2024-04-30 16:30:33 +0800

    Update movePlayer.cpp

--- movePlayer.cpp
+++ movePlayer.cpp
@@ -4,20 +4,18 @@
 
 using namespace std;
 
-void movePlayer(int width, int height, Player &P, char **map,int ch, int &dirX, int &dirY, int &beansCount/*int &sleep*/){
+void movePlayer(int width, int height, Player &P, char **map,int ch, int &dirX, int &dirY, int &beansCount){
     if (ch != ERR) {
         switch (ch) {
             case 'w':
-                // 上移逻辑
-                //std::cout << "检测到w移动"<<std::endl;
+
                 if (dirX!=0){
                     dirX=0;
                     dirY=-1;
                     }
                 break;
             case 's':
-                // 下移逻辑
-                //std::cout << "检测到s移动"<<std::endl;
+
                 if (dirX!=0){
                     dirX=0;
                     dirY=1;
@@ -25,16 +23,14 @@ void movePlayer(int width, int height, Player &P, char **map,int ch, int &dirX, 
 
                 break;
             case 'a':
-                // 左移逻辑
-                //std::cout << "检测到a移动"<<std::endl;
+
                 if (dirY!=0){
                     dirX=-1;
                     dirY=0;
                 }
                 break;
             case 'd':
-                // 右移逻辑
-               // std::cout << "检测到d移动"<<std::endl;
+
                 if (dirY!=0){
                     dirX=1;
                     dirY=0;
