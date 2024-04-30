cd9abf4e294b46504ccae71ec695fb1c6a4ed896
Pzmm0808 <158024565+Pzmm0808@users.noreply.github.com>
2024-04-30 16:34:44 +0800

    Update randomBeam.h

--- randomBeam.h
+++ randomBeam.h
@@ -5,12 +5,10 @@
 #include <ctime>
 #include "player.h"
 
-// 随机生成数字的函数
+
 int randomInt(int min, int max);
 
-// 随机在地图上生成豆子的函数
 void generateBean(int width, int height, const Player &P, char **map);
 
-// 判断生成地点是否有Player
 int isPlayerPosition(int playerX, int playerY, int beanX, int beanY);
 #endif
