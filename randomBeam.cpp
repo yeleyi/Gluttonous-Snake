dd14263e99ac0c2b9df3d600ee16a24cd034bc6c
yeleyi <167627264+yeleyi@users.noreply.github.com>
2024-04-30 13:26:29 +0800

    Update randomBeam.cpp

--- randomBeam.cpp
+++ randomBeam.cpp
@@ -1,23 +1,23 @@
 #include "randomBeam.h"
   
 int randomInt(int min, int max){
-        static bool initialized = false;
-        if (!initialized){
-                srand(time(NULL));
-                initialized = true;
-        }
-        return min + rand() % (max -min + 1);
+static bool initialized = false;
+  if (!initialized){
+    srand(time(NULL));
+    initialized = true;
+  }
+  return min + rand() % (max -min + 1);
 }
 // 随机在地图上生成豆子的函数
 void generateBean(int width, int height, const Player &P, char **map) {
-    int beanX, beanY;
+  int beanX, beanY;
 
-    do{
-        beanX = randomInt(1, width -2);
-        beanY = randomInt(1, height-2);
-    } while (map[beanX][beanY] != '.');
-    // 放置bean
-    map[beanX][beanY] = 'B';
+  do{
+    beanX = randomInt(1, width -2);
+    beanY = randomInt(1, height-2);
+  } while (map[beanX][beanY] != '.');
+  // 放置bean
+  map[beanX][beanY] = 'B';
 }
 
 // 判断生成地点是否有Player
