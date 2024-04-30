4296f40bcd9c7a76bc1b21221ac7982cc79c4312
yeleyi <167627264+yeleyi@users.noreply.github.com>
2024-04-30 13:27:10 +0800

    Update renderMap.cpp

--- renderMap.cpp
+++ renderMap.cpp
@@ -4,13 +4,13 @@ void renderMap(int width, int height, char **map, Player &P){
   clear();
   for (int y=0; y<height; ++y){
     for (int x=0; x<width; ++x){
-            printw("%c", map[y][x]);
+      printw("%c", map[y][x]);
     }
     printw("\n");
   }
 
   for (int i=1; i<P.body.size(); ++i){
-        map[P.body[i].second][P.body[i].first] = 'T';
+    map[P.body[i].second][P.body[i].first] = 'T';
   }
 
   refresh(); // 刷新屏幕
