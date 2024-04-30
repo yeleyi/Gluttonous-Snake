d2d1058299be5d7f6a7ef1ecae4c8d0bed1b248f
Pzmm0808 <158024565+Pzmm0808@users.noreply.github.com>
2024-04-30 16:34:11 +0800

    Update randomBeam.cpp

--- randomBeam.cpp
+++ randomBeam.cpp
@@ -8,7 +8,7 @@ static bool initialized = false;
   }
   return min + rand() % (max -min + 1);
 }
-// 随机在地图上生成豆子的函数
+
 void generateBean(int width, int height, const Player &P, char **map) {
   int beanX, beanY;
 
@@ -16,11 +16,11 @@ void generateBean(int width, int height, const Player &P, char **map) {
     beanX = randomInt(1, width -2);
     beanY = randomInt(1, height-2);
   } while (map[beanX][beanY] != '.');
-  // 放置bean
+
   map[beanX][beanY] = 'B';
 }
 
-// 判断生成地点是否有Player
+
 int isPlayerPosition(int playerX, int playerY, int beanX, int beanY){
   return (playerX != beanX && playerY != beanY);
 }
