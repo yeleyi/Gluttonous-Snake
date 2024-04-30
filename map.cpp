937064456cb212cdc34e63268df67ddfff50eeb1
Pzmm0808 <158024565+Pzmm0808@users.noreply.github.com>
2024-04-30 20:41:31 +0800

    Update map.cpp

--- map.cpp
+++ map.cpp
@@ -1,12 +1,12 @@
 #include "map.h"
+
+// generate map
 void mapf(int width, int height, char ** map){
     for (int y = 0; y < height; ++y) {
         for (int x = 0; x < width; ++x) {
             map[y][x] = '.';
         }
     }
-
-
     for (int x = 0; x < width; ++x) {
         map[0][x] = '#';
         map[height - 1][x] = '#';
